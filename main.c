43f645b89bccd6ee64ef5e0b2c4bd11ae584f75f
Luzog78 <luzog78@gmail.com>
2023-09-23 02:28:43 +0200

    Working Raycasting !

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <SDL2/SDL.h>
+
+#define PI 3.14159265358979323846
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 #define SCREEN_MAX_INDEX (SCREEN_WIDTH * SCREEN_HEIGHT * 4)
+#define RAYS_AMOUNT 512
+#define RAYS_DISPLAY 10
+#define RAYS_FOV (PI / 3)
+#define RAYS_MAX_DISTANCE 100
 
 typedef struct
 {
-	int x;
-	int y;
-} s_point;
+	float x;
+	float y;
+} t_vec2;
 
 typedef struct
 {
@@ -17,30 +25,66 @@ typedef struct
 	Uint8 g;
 	Uint8 b;
 	Uint8 a;
-} s_color;
+} t_color;
+
+typedef struct
+{
+	int width;
+	int height;
+	int scale;
+	Uint8 *array;
+} t_sdl_canvas;
+
+typedef struct
+{
+	int width;
+	int height;
+	int *array;
+} t_level;
+
+typedef struct
+{
+	t_vec2 position;
+	float angle;
+	float distance;
+	int side;
+} t_ray;
+
+typedef struct
+{
+	t_vec2 position;
+	float direction;
+	float speed;
+	t_ray rays[RAYS_AMOUNT];
+} t_player;
 
 typedef struct
 {
 	SDL_Window *window;
+	t_sdl_canvas screen;
+	t_sdl_canvas minimap;
 	SDL_Renderer *renderer;
 	SDL_Texture *texture;
-	Uint8 screen[SCREEN_WIDTH * SCREEN_HEIGHT * 4];
-} s_sdl_master;
+	t_level level;
+	t_player player;
+	Uint64 clock;
+} t_sdl_master;
 
-void screen_draw_pixel(s_sdl_master *master, s_point *point, s_color *color)
+void screen_draw_pixel(t_sdl_canvas *canvas, t_vec2 *point, t_color *color)
 {
-	int index = (point->y * SCREEN_WIDTH + point->x) * 4;
-	if (index >= 0 && index < SCREEN_MAX_INDEX)
-		master->screen[index] = color->r;
-	if (index + 1 >= 0 && index + 1 < SCREEN_MAX_INDEX)
-		master->screen[index + 1] = color->g;
-	if (index + 2 >= 0 && index + 2 < SCREEN_MAX_INDEX)
-		master->screen[index + 2] = color->b;
-	if (index + 3 >= 0 && index + 3 < SCREEN_MAX_INDEX)
-		master->screen[index + 3] = color->a;
+	int size = canvas->width * canvas->height * 4;
+	int index = (point->y * canvas->width + point->x) * 4;
+	if (index >= 0 && index + 3 < size)
+	{
+		t_color pixel = {canvas->array[index], canvas->array[index + 1], canvas->array[index + 2], canvas->array[index + 3]};
+		canvas->array[index] = (pixel.r * (255 - color->a) + color->r * color->a) / 255;
+		canvas->array[index + 1] = (pixel.g * (255 - color->a) + color->g * color->a) / 255;
+		canvas->array[index + 2] = (pixel.b * (255 - color->a) + color->b * color->a) / 255;
+		canvas->array[index + 3] = 255 - ((255 - pixel.a) * (255 - color->a) / 255);
+	}
 }
 
-void screen_draw_line(s_sdl_master *master, s_point *point1, s_point *point2, s_color *color)
+void screen_draw_line(t_sdl_canvas *canvas, t_vec2 *point1, t_vec2 *point2, t_color *color)
 {
 	int dx = point2->x - point1->x;
 	int dy = point2->y - point1->y;
@@ -51,14 +95,14 @@ void screen_draw_line(s_sdl_master *master, s_point *point1, s_point *point2, s_
 	float y = point1->y;
 	for (int i = 0; i <= steps; i++)
 	{
-		s_point point = {(int) x, (int) y};
-		screen_draw_pixel(master, &point, color);
+		t_vec2 point = {(int) x, (int) y};
+		screen_draw_pixel(canvas, &point, color);
 		x += x_inc;
 		y += y_inc;
 	}
 }
 
-void screen_draw_rect(s_sdl_master *master, s_point *point1, s_point *point2, s_color *color, int fill)
+void screen_draw_rect(t_sdl_canvas *canvas, t_vec2 *point1, t_vec2 *point2, t_color *color, int fill)
 {
 	for (int y = point1->y; y <= point2->y; y++)
 	{
@@ -66,14 +110,14 @@ void screen_draw_rect(s_sdl_master *master, s_point *point1, s_point *point2, s_
 		{
 			if (fill == 1 || x == point1->x || x == point2->x || y == point1->y || y == point2->y)
 			{
-				s_point point = {x, y};
-				screen_draw_pixel(master, &point, color);
+				t_vec2 point = {x, y};
+				screen_draw_pixel(canvas, &point, color);
 			}
 		}
 	}
 }
 
-void screen_draw_circle(s_sdl_master *master, s_point *point1, int radius, s_color *color, int fill)
+void screen_draw_circle(t_sdl_canvas *canvas, t_vec2 *point1, int radius, t_color *color, int fill)
 {
 	int x = radius;
 	int y = 0;
@@ -82,21 +126,21 @@ void screen_draw_circle(s_sdl_master *master, s_point *point1, int radius, s_col
 	{
 		if (fill == 1)
 		{
-			screen_draw_line(master, &(s_point){point1->x - x, point1->y + y}, &(s_point){point1->x + x, point1->y + y}, color);
-			screen_draw_line(master, &(s_point){point1->x - y, point1->y + x}, &(s_point){point1->x + y, point1->y + x}, color);
-			screen_draw_line(master, &(s_point){point1->x - x, point1->y - y}, &(s_point){point1->x + x, point1->y - y}, color);
-			screen_draw_line(master, &(s_point){point1->x - y, point1->y - x}, &(s_point){point1->x + y, point1->y - x}, color);
+			screen_draw_line(canvas, &(t_vec2){point1->x - x, point1->y + y}, &(t_vec2){point1->x + x, point1->y + y}, color);
+			screen_draw_line(canvas, &(t_vec2){point1->x - y, point1->y + x}, &(t_vec2){point1->x + y, point1->y + x}, color);
+			screen_draw_line(canvas, &(t_vec2){point1->x - x, point1->y - y}, &(t_vec2){point1->x + x, point1->y - y}, color);
+			screen_draw_line(canvas, &(t_vec2){point1->x - y, point1->y - x}, &(t_vec2){point1->x + y, point1->y - x}, color);
 		}
 		else
 		{
-			screen_draw_pixel(master, &(s_point){point1->x + x, point1->y + y}, color);
-			screen_draw_pixel(master, &(s_point){point1->x + y, point1->y + x}, color);
-			screen_draw_pixel(master, &(s_point){point1->x - y, point1->y + x}, color);
-			screen_draw_pixel(master, &(s_point){point1->x - x, point1->y + y}, color);
-			screen_draw_pixel(master, &(s_point){point1->x - x, point1->y - y}, color);
-			screen_draw_pixel(master, &(s_point){point1->x - y, point1->y - x}, color);
-			screen_draw_pixel(master, &(s_point){point1->x + y, point1->y - x}, color);
-			screen_draw_pixel(master, &(s_point){point1->x + x, point1->y - y}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x + x, point1->y + y}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x + y, point1->y + x}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x - y, point1->y + x}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x - x, point1->y + y}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x - x, point1->y - y}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x - y, point1->y - x}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x + y, point1->y - x}, color);
+			screen_draw_pixel(canvas, &(t_vec2){point1->x + x, point1->y - y}, color);
 		}
 		y++;
 		if (radius_error < 0)
@@ -111,15 +155,48 @@ void screen_draw_circle(s_sdl_master *master, s_point *point1, int radius, s_col
 	}
 }
 
-int init(s_sdl_master *master)
+int init(t_sdl_master *master)
 {
 	master->window = NULL;
 	master->renderer = NULL;
 	master->texture = NULL;
-	for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT * 4; i++)
+	master->screen.width = SCREEN_WIDTH;
+	master->screen.height = SCREEN_HEIGHT;
+	master->screen.scale = 1;
+	master->screen.array = malloc(master->screen.width * master->screen.height * 4 * sizeof(Uint8));
+	master->level.width = 8;
+	master->level.height = 8;
+	master->level.array = malloc(master->level.width * master->level.height * sizeof(int));
+	master->minimap.width = master->level.width * 24;
+	master->minimap.height = master->level.height * 24;
+	master->minimap.scale = 1;
+	master->minimap.array = malloc(master->minimap.width * master->minimap.height * 4 * sizeof(Uint8));
+	master->player.position = (t_vec2){3.5, 5.5};
+	master->player.direction = 0;
+	master->player.speed = 0.1;
+	master->clock = 0;
+
+	if (master->screen.array == NULL || master->minimap.array == NULL || master->level.array == NULL)
 	{
-		master->screen[i] = (i % 4) == 3 ? 255 : 0;
+		printf("malloc Error.\n");
+		return 1;
 	}
+
+	for (int i = 0; i < master->screen.width * master->screen.height * 4; i++)
+		master->screen.array[i] = (i % 4) == 3 ? 255 : 0;
+	for (int i = 0; i < master->minimap.width * master->minimap.height * 4; i++)
+		master->minimap.array[i] = (i % 4) == 3 ? 255 : 0;
+	for (int i = 0; i < master->level.width * master->level.height; i++)
+		master->level.array[i] = (int[]){
+			1, 1, 1, 1, 1, 1, 1, 1,
+			1, 0, 1, 0, 0, 0, 0, 1,
+			1, 0, 1, 0, 0, 0, 0, 1,
+			1, 0, 1, 0, 1, 1, 0, 1,
+			1, 0, 0, 0, 0, 1, 0, 1,
+			1, 0, 0, 0, 0, 0, 0, 1,
+			1, 0, 0, 0, 0, 0, 0, 1,
+			1, 1, 1, 1, 1, 1, 1, 1
+		}[i];
 
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
@@ -142,8 +219,9 @@ int init(s_sdl_master *master)
 		return 1;
 	}
 
-	master->texture = SDL_CreateTexture(master->renderer, SDL_PIXELFORMAT_RGBA32,
-										SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+	master->texture = SDL_CreateTexture(master->renderer,
+								SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING,
+								SCREEN_WIDTH, SCREEN_HEIGHT);
 	if (master->texture == NULL)
 	{
 		printf("SDL_CreateTexture Error: %s\n", SDL_GetError());
@@ -153,8 +231,20 @@ int init(s_sdl_master *master)
 	return 0;
 }
 
-void quit(int exit_code, s_sdl_master *master)
+void quit(int exit_code, t_sdl_master *master)
 {
+	if (master->screen.array != NULL)
+	{
+		free(master->screen.array);
+	}
+	if (master->minimap.array != NULL)
+	{
+		free(master->minimap.array);
+	}
+	if (master->level.array != NULL)
+	{
+		free(master->level.array);
+	}
 	if (master->window != NULL)
 	{
 		SDL_DestroyWindow(master->window);
@@ -172,24 +262,121 @@ void quit(int exit_code, s_sdl_master *master)
 	exit(0);
 }
 
-void update_screen(s_sdl_master *master)
+void update_canvas(int size, int width, Uint8 **pixels, t_sdl_canvas *canvas)
 {
-	SDL_UpdateTexture(master->texture, NULL, master->screen, SCREEN_WIDTH * 4 * sizeof(Uint8));
+	for (int y = 0; y < canvas->height * canvas->scale; y++)
+	{
+		for (int x = 0; x < canvas->width * canvas->scale; x++)
+		{
+			int index = (((int) (y / canvas->scale)) * canvas->width + ((int) (x / canvas->scale))) * 4 ;
+			int window_index = (y * width + x) * 4;
+			if (window_index >= 0 && window_index + 3 < size)
+			{
+				(*pixels)[window_index] = ((*pixels)[window_index] * (255 - canvas->array[index + 3]) + canvas->array[index] * canvas->array[index + 3]) / 255;
+				(*pixels)[window_index + 1] = ((*pixels)[window_index + 1] * (255 - canvas->array[index + 3]) + canvas->array[index + 1] * canvas->array[index + 3]) / 255;
+				(*pixels)[window_index + 2] = ((*pixels)[window_index + 2] * (255 - canvas->array[index + 3]) + canvas->array[index + 2] * canvas->array[index + 3]) / 255;
+				(*pixels)[window_index + 3] = 255 - ((255 - (*pixels)[window_index + 3]) * (255 - canvas->array[index + 3]) / 255);
+			}
+		}
+	}
+}
+
+void update_window(t_sdl_master *master)
+{
+	int size = SCREEN_WIDTH * SCREEN_HEIGHT * 4;
+	Uint8 *pixels = malloc(size * sizeof(Uint8));
+	if (pixels == NULL)
+	{
+		printf("malloc Error.\n");
+		quit(1, NULL);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		pixels[i] = 0;
+	}
+	update_canvas(size, SCREEN_WIDTH, &pixels, &master->screen);
+	update_canvas(size, SCREEN_WIDTH, &pixels, &master->minimap);
+	SDL_UpdateTexture(master->texture, NULL, pixels, SCREEN_WIDTH * 4 * sizeof(Uint8));
 	SDL_RenderClear(master->renderer);
 	SDL_RenderCopy(master->renderer, master->texture, NULL, NULL);
 	SDL_RenderPresent(master->renderer);
+	free(pixels);
+}
+
+void update_minimap(t_sdl_master *master)
+{
+	for (int i = 0; i < master->minimap.width * master->minimap.height * 4; i++)
+	{
+		master->minimap.array[i] = (i % 4) == 3 ? 255 : 0;
+	}
+
+	for (int y = 0; y < master->level.height; y++)
+	{
+		for (int x = 0; x < master->level.width; x++)
+		{
+			int index = y * master->level.width + x;
+			if (master->level.array[index] == 1)
+			{
+				screen_draw_rect(&master->minimap,
+					&(t_vec2){x * 24, y * 24}, &(t_vec2){x * 24 + 24, y * 24 + 24}, &(t_color){255, 0, 0, 196}, 1);
+			}
+		}
+	}
+
+	for (int i = 0; i < RAYS_DISPLAY; i++)
+	{
+		t_ray ray = master->player.rays[i * RAYS_AMOUNT / RAYS_DISPLAY];
+		screen_draw_line(&master->minimap,
+			&(t_vec2){master->player.position.x * 24, master->player.position.y * 24},
+			&(t_vec2){ray.position.x * 24, ray.position.y * 24},
+			&(t_color){255, 255, 0, 255});
+	}
+
+	screen_draw_line(&master->minimap,
+		&(t_vec2){master->player.position.x * 24, master->player.position.y * 24},
+		&(t_vec2){master->player.position.x * 24 + 24 * cos(master->player.direction), master->player.position.y * 24 + 24 * sin(master->player.direction)},
+		&(t_color){255, 255, 255, 255});
+	screen_draw_circle(&master->minimap,
+		&(t_vec2){master->player.position.x * 24, master->player.position.y * 24},
+		6, &(t_color){0, 255, 255, 255}, 1);
+}
+
+void update_screen(t_sdl_master *master)
+{
+	for (int i = 0; i < master->screen.width * master->screen.height * 4; i++)
+	{
+		master->screen.array[i] = (i % 4) == 3 ? 255 : 0;
+	}
+
+	screen_draw_rect(&master->screen,
+		&(t_vec2){0, 0}, &(t_vec2){master->screen.width, master->screen.height / 2},
+		&(t_color){0, 128, 255, 255}, 1);
+	screen_draw_rect(&master->screen,
+		&(t_vec2){0, master->screen.height / 2}, &(t_vec2){master->screen.width, master->screen.height},
+		&(t_color){170, 85, 0, 255}, 1);
+
+	for (int i = 0; i < RAYS_AMOUNT; i++)
+	{
+		t_ray ray = master->player.rays[i];
+		float distance = ray.distance * cos(ray.angle - master->player.direction);
+		float height = master->screen.height / distance / 1.5;
+		float position = (master->screen.height - height) / 2.0;
+		float tiling = master->screen.width / (float) RAYS_AMOUNT;
+		screen_draw_rect(&master->screen,
+			&(t_vec2){i * tiling, position},
+			&(t_vec2){i * tiling + tiling, position + height},
+			ray.side == 1 ? &(t_color){255, 255, 255, 255} : &(t_color){225, 225, 225, 255}, 1);
+	}
 }
 
 int main()
 {
-	s_sdl_master master;
+	t_sdl_master master;
 
 	if (init(&master) != 0)
 	{
 		quit(1, &master);
 	}
-
-	int i = 0;
 
 	while (1)
 	{
@@ -202,17 +389,93 @@ int main()
 			}
 		}
 
-		if (i >= 255)
-			i = -255;
-		else
-			i += 8;
+		const Uint8 *state = SDL_GetKeyboardState(NULL);
+		if (state[SDL_SCANCODE_ESCAPE])
+		{
+			break;
+		}
+		if (state[SDL_SCANCODE_UP])
+		{
+			master.player.position.x += master.player.speed * cos(master.player.direction);
+			master.player.position.y += master.player.speed * sin(master.player.direction);
+		}
+		if (state[SDL_SCANCODE_DOWN])
+		{
+			master.player.position.x -= master.player.speed * cos(master.player.direction);
+			master.player.position.y -= master.player.speed * sin(master.player.direction);
+		}
+		if (state[SDL_SCANCODE_LEFT])
+		{
+			master.player.direction -= 0.1;
+		}
+		if (state[SDL_SCANCODE_RIGHT])
+		{
+			master.player.direction += 0.1;
+		}
 
-		s_color color = {0, i < 0 ? -i - 1 : i, 0, 255};
-		screen_draw_rect(&master, &(s_point){0, 0}, &(s_point){SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1}, &color, 0);
+		for (int i = 0; i < RAYS_AMOUNT; i++)
+		{
+			float angle = master.player.direction - RAYS_FOV / 2 + i * RAYS_FOV / RAYS_AMOUNT;
+			while (angle < 0)
+				angle += 2 * PI;
+			while (angle >= 2 * PI)
+				angle -= 2 * PI;
 
+			float dx, dy;
+
+			t_vec2 vertical_position;
+			float atan = -1 / tan(angle);
+			vertical_position.y = (int) master.player.position.y + (angle >= PI ? 0 : 1);
+			vertical_position.x = master.player.position.x + (master.player.position.y - vertical_position.y) * atan;
+			dy = (angle >= PI ? -1 : 1);
+			dx = -dy * atan;
+			while (vertical_position.y >= 0 && vertical_position.y < master.level.height)
+			{
+				int index = ((int) (vertical_position.y + (angle >= PI ? -1 : 0))) * master.level.width + (int) (vertical_position.x);
+				if (index < 0 || index >= master.level.width * master.level.height)
+					break;
+				if (master.level.array[index] == 1)
+				{
+					break;
+				}
+				vertical_position.y += dy;
+				vertical_position.x += dx;
+			}
+			float vertical_distance = sqrt(pow(vertical_position.x - master.player.position.x, 2) + pow(vertical_position.y - master.player.position.y, 2));
+
+			t_vec2 horizontal_position;
+			float ntan = -tan(angle);
+			horizontal_position.x = (int) master.player.position.x + (angle >= PI / 2 && angle < 3 * PI / 2 ? 0 : 1);
+			horizontal_position.y = master.player.position.y + (master.player.position.x - horizontal_position.x) * ntan;
+			dx = (angle >= PI / 2 && angle < 3 * PI / 2 ? -1 : 1);
+			dy = -dx * ntan;
+			while (horizontal_position.x >= 0 && horizontal_position.x < master.level.width)
+			{
+				int index = ((int) (horizontal_position.y)) * master.level.width + (int) (horizontal_position.x + (angle >= PI / 2 && angle < 3 * PI / 2 ? -1 : 0));
+				if (index < 0 || index >= master.level.width * master.level.height)
+					break;
+				if (master.level.array[index] == 1)
+				{
+					break;
+				}
+				horizontal_position.x += dx;
+				horizontal_position.y += dy;
+			}
+			float horizontal_distance = sqrt(pow(horizontal_position.x - master.player.position.x, 2) + pow(horizontal_position.y - master.player.position.y, 2));
+
+			master.player.rays[i].position = vertical_distance < horizontal_distance ? vertical_position : horizontal_position;
+			master.player.rays[i].angle = angle;
+			master.player.rays[i].distance = vertical_distance < horizontal_distance ? vertical_distance : horizontal_distance;
+			master.player.rays[i].side = vertical_distance < horizontal_distance ? 0 : 1;
+		}
+
+		update_minimap(&master);
 		update_screen(&master);
-	}
 
+		master.clock++;
+		SDL_Delay(1000 / 60);
+		update_window(&master);
+	}
 	
 	quit(0, &master);
 	return 0;
